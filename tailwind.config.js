8ea6fc8aa6c870599b8c12a7717d6f4488e07600
Anderson <laventurepsu1@gmail.com>
2021-02-24 15:36:48 -0500

    updated tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,6 @@ module.exports = {
         "brown-light": "#97613",
         "brown-dark": "#603e26",
         "teal-light": "#9af6f6",
-        "teal-dark": "#5c9393",
         "pink-light": "#f05454",
         "purple-light": "#e5c3fe",
         "purple-dark": "#ab3afc",
